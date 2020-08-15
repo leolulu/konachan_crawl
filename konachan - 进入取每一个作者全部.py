c6264fa5bdc3e975c52bb2e573c8eb9289ae0e82
surface@pro.3 <348699103@qq.com>
2020-08-15 18:25:59 +0800

    优化了详情页和下载之间的逻辑

--- konachan - 进入取每一个作者全部.py
+++ konachan - 进入取每一个作者全部.py
@@ -15,7 +15,7 @@ class KonachanInner:
         self.page_capacity = page_capacity
         self.except_artist_tag = 'tagme (artist)'
         self.page_num = 1
-        self.base_url = 'http://konachan.com/post'
+        self.base_url = 'http://konachan.net/post'
         self.headers = {
             "Cookie": "vote=1; __utmz=20658210.1460984814.55.2.utmcsr=konachan.com|utmccn=(referral)|utmcmd=referral|utmcct=/post/switch; __cfduid=d18af1d27bb5882a6eff521053cb3cc801525011444; tag-script=; country=US; blacklisted_tags=%5B%22%22%5D; konachan.net=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJTlhYTcwNjk4YzI4NjdjYWU2YjZhYzg2YTZiOWRlZmQ1BjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMTIxZXdwajVzYVRuR0huSWtWWEUrdlJPOE1EMUdCMHdUdG5yMjFXNmNVNm89BjsARg%3D%3D--8a1e71bdae987934cb60ab31c927084b3c5d85c6; __utmc=20658210; Hm_lvt_ba7c84ce230944c13900faeba642b2b4=1536069861,1536796970,1536939737,1537279770; __utma=20658210.97867196.1446035811.1537370253.1537509624.843; __utmt=1; forum_post_last_read_at=%222018-09-21T08%3A00%3A31%2B02%3A00%22; __utmb=20658210.3.10.1537509624; Hm_lpvt_ba7c84ce230944c13900faeba642b2b4=1537509630",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
@@ -94,26 +94,22 @@ class KonachanInner:
         """
         解析详情页
         """
-        for detail_page_url in tqdm(detail_page_url_list):
-            html = self.parse_url(detail_page_url)
-            artist_name = html.xpath("//li[contains(@class,'tag-type-artist')]/a[2]/text()")[0]
-            artist_url = self.url_compliet(html.xpath("//li[contains(@class,'tag-type-artist')]/a[2]/@href")[0])
-            # orig_img_url = html.xpath("//div[@class='sidebar']//a[contains(@class,'highres-show')]/@href")[0]
-            if deal_func(artist_name) is not None:
-                # 1.点击作者进入作者图像列表
-                next_url = artist_url
-                while next_url is not None:
-                    html = self.parse_url(next_url)
-                    next_url = self.url_compliet(self.extract(html.xpath("//a[text()='Next →']/@href")))
-                    img_url_list = html.xpath("//ul[@id='post-list-posts']/li/a/@href")
-                    # self.download_img(img_url_list)
-                    # 2.获取图像
-                    with ThreadPoolExecutor(max_workers=8) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            for detail_page_url in tqdm(detail_page_url_list):
+                html = self.parse_url(detail_page_url)
+                artist_name = html.xpath("//li[contains(@class,'tag-type-artist')]/a[2]/text()")[0]
+                artist_url = self.url_compliet(html.xpath("//li[contains(@class,'tag-type-artist')]/a[2]/@href")[0])
+                # orig_img_url = html.xpath("//div[@class='sidebar']//a[contains(@class,'highres-show')]/@href")[0]
+                if deal_func(artist_name) is not None:
+                    # 1.点击作者进入作者图像列表
+                    next_url = artist_url
+                    while next_url is not None:
+                        html = self.parse_url(next_url)
+                        next_url = self.url_compliet(self.extract(html.xpath("//a[text()='Next →']/@href")))
+                        img_url_list = html.xpath("//ul[@id='post-list-posts']/li/a/@href")
+                        # 2.获取图像
                         for img_url in img_url_list:
                             executor.submit(self.downloadPic, img_url, artist_name)
-                    # for img_url in img_url_list:
-                    #     print('downloading:', img_url, artist_name)
-                    #     self.downloadPic(img_url, artist_name)
 
     @retry(wait_exponential_multiplier=1000, wait_exponential_max=60000)
     def downloadPic(self, img_url, artist_name):
@@ -166,5 +162,5 @@ class pickle_handler:
 
 
 if __name__ == "__main__":
-    k1 = KonachanInner(2)
+    k1 = KonachanInner(15)
     k1.run()
